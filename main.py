fbc4518f1f3a54443ca4b7532cdb0bca5f6ed37a
yash14700 <44956243+yash14700@users.noreply.github.com>
2021-04-09 09:03:12 +0400

    plan for backend endpoints

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ Flask endpoints to make
     uniqname
 -> What it does
  -> output
+    {user_id: ''}
     success
 2. Return List of questions they have to do (18)
+output: [
+        {question_id: int, topic:"", option_1: {embed_link:"", transcript:""}, option_2:{...},...},
+        {...},
+        ..
+        ]
 3. Submit answers to a given question
+input: {
+            question_id:
+            user_id:
+            best_option:
+            worst_option:
+        }
 """
